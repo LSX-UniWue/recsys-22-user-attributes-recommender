b2c7a202a6b0b40d61c500fa354f0f01acdc58a2
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2020-10-22 18:53:03 +0200

    fixing caser model, should work now
    pair programming with alex and the cool code with me plugin from jetbrains

--- metrics/ranking_metrics.py
+++ metrics/ranking_metrics.py
@@ -2,9 +2,6 @@ import pytorch_lightning as pl
 import torch
 
 
-# TODO (AD): For Transformer models we could evaluate the whole sequence at once
-
-
 class RecallAtMetric(pl.metrics.Metric):
     def __init__(self,
                  k: int,
@@ -18,19 +15,33 @@ class RecallAtMetric(pl.metrics.Metric):
 
     def update(self,
                prediction: torch.Tensor,
-               target: torch.Tensor
+               target: torch.Tensor,
+               mask: torch.Tensor = None,
                ) -> torch.Tensor:
         """
-        :param prediction: the scores of all items, the first one is the positive item, all others are negatives
-        :param target: the target label tensor
-        :return: the recall at specified k
+        :param prediction: the scores of all items :math `(N, I)`
+        :param target: the target label tensor :math `(N, T)` or :math `(N)`
+        :param mask: the mask to apply, iff no mask is provided all targets are used for calculating the metric
+        :math `(N, I)`
         """
-        sorted_indices = torch.topk(prediction, k=self._k)[1]
-        target_expanded = target.view(-1, 1).expand(-1, self._k)
 
-        # FIXME (AD) make it work for target vectors with size > 1
-        tp = sorted_indices.eq(target_expanded).sum(dim=-1).to(dtype=torch.float)
-        fn = torch.ones(tp.size(), device=tp.device) - tp
+        if len(target.size()) == 1:
+            # single dimension, unsqueeze it
+            target = torch.unsqueeze(target, 1)
+
+        if mask is None:
+            mask = torch.ones(target.size())
+
+        sorted_indices = torch.topk(prediction, k=self._k).indices
+        sorted_indices = torch.repeat_interleave(sorted_indices, mask.size()[-1], dim=0)
+
+        target_expanded = target.view(-1, 1).expand(-1, self._k)
+        tp_mask = torch.repeat_interleave(mask.view(-1, 1), self._k, dim=1)
+
+        tp = (sorted_indices.eq(target_expanded) * tp_mask).sum(dim=-1).to(dtype=torch.float)
+        tp = tp.view(mask.size()).sum(-1)
+
+        fn = mask.sum(-1) - tp
 
         recall = tp / (tp + fn)
         self.recall += recall.sum()
@@ -58,14 +69,14 @@ class MRRAtMetric(pl.metrics.Metric):
                target: torch.Tensor
                ) -> torch.Tensor:
         """
-        :param prediction: the scores of all items, the first one is the positive item, all others are negatives
-        :param target: the target label tensor
-        :return: the recall at specified k
+        :param prediction: the scores of all items, the first one is the positive item, all others are negatives :math `(N, I)`
+        :param target: the target label tensor :math `(N, T)`
+        :return: the mrr at specified k
         """
-        sorted_indices = torch.topk(prediction, k=self._k)[1]
+        sorted_indices = torch.topk(prediction, k=self._k).indices
         target = target.view(-1, 1).expand(-1, self._k)
 
-        rank = torch.topk((sorted_indices.eq(target) * torch.arange(1, self._k + 1, dtype=torch.float, device=target.device)), k=1)[0]
+        rank = torch.topk((sorted_indices.eq(target) * torch.arange(1, self._k + 1, dtype=torch.float, device=target.device)), k=1).values
 
         # mrr will contain 'inf' values if target is not in topk scores -> setting to 0
         mrr = 1 / rank
@@ -79,15 +90,40 @@ class MRRAtMetric(pl.metrics.Metric):
 
 
 if __name__ == "__main__":
-    prediction = torch.tensor([
-        [0.5, 0.12, 0.3, 0.18],
-        [0.5, 0.12, 0.3, 0.18],
+    test_prediction = torch.tensor([
+        [0.5, 0.12, 0.3, 0.18],  # ranking: 0, 2, 3, 1
+        [0.5, 0.12, 0.6, 0.18],  # ranking: 2, 0, 3, 1
     ])
-    target = torch.tensor([[2], [0]])
+    test_target = torch.tensor([[0], [12]])  # assume arbitrary masking id
+    #mask = torch.tensor([[1, 1], [1, 1]])
+    # k=3
+    # max_target_size = 2
+    #
+    # if max_target_size > k:
+    #     raise Exception()
+    #
+    # sorted_indices = torch.topk(prediction, k=k)[1]
+    # sorted_indices = torch.repeat_interleave(sorted_indices, max_target_size, dim=0)
+    #
+    #
+    # print(sorted_indices)
+    #
+    # target = torch.tensor([[2, 1], [0, 3]])  # assume arbitrary masking id
+    # mask = torch.tensor([[1, 1], [1, 0]])  # mask marks last entry in target as padded
+    #
+    #
+    # target = target.view(-1, 1)
+    # print(target)
+    # target = target.expand(-1, k)
+    # print(target)
+    #
+    # rank = torch.topk((sorted_indices.eq(target) * torch.arange(1, k + 1, dtype=torch.float, device=target.device)), k=k)[0]
+    # print(rank)
+    #
+    # rank = rank * mask.view([1, -1])
+    # print(rank)
 
-    metric = MRRAtMetric(k=3)
-    metric(prediction, target)
-
-    target = torch.tensor([[1], [1]])
-    metric(prediction, target)
+    metric = RecallAtMetric(k=1)
+    metric(test_prediction, test_target)
     print(metric.compute())
+
