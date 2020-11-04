3a4ff6ef3169bc3058936b8d5f2fa73abc11a0d9
Alexander Dallmann <dallman@informatik.uni-wuerzburg.de>
2020-11-04 13:21:33 +0100

    bump pytorch-lightning to 1.0.5 add dependency for typer rewrite next item index script with typer

--- runner/dataset/create_next_item_index.py
+++ runner/dataset/create_next_item_index.py
@@ -2,47 +2,41 @@ from argparse import ArgumentParser
 from pathlib import Path
 from typing import Text
 
+import typer
+
 from data.base.reader import CsvDatasetIndex, CsvDatasetReader
 from data.datasets.nextitem import NextItemIndexBuilder
 from data.datasets.session import ItemSessionDataset, ItemSessionParser
 from data.utils import create_indexed_header, read_csv_header
 
+app = typer.Typer()
 
-def run(data_file_path: Path, session_index_path: Path, output_file_path: Path, item_header_name: Text, min_session_length: int = 2, delimiter: Text = "\t"):
 
-    session_index = CsvDatasetIndex(session_index_path)
-    print(len(session_index))
+@app.command()
+def run(data: Path = typer.Argument(..., help="path to the input file in CSV format"),
+        session_index: Path = typer.Argument(..., help="path to the session index file"),
+        output: Path = typer.Argument(..., help="path to the output file"),
+        item_header_name: Text = typer.Argument(..., help="name of the column that contains the item id"),
+        min_session_length: int = typer.Option(2, help="the minimum acceptable session length"),
+        delimiter: Text = typer.Option("\t", help="the delimiter used in the CSV file.")
+        ):
+    """
+    Creates an index pointing to samples for the next item prediction task by adding a distinct sample for every
+    target item in each session to the generated index.
+    """
 
-    reader = CsvDatasetReader(data_file_path, session_index)
-    print(len(reader))
+    session_index = CsvDatasetIndex(session_index)
+
+    reader = CsvDatasetReader(data, session_index)
     session_parser = ItemSessionParser(
-        create_indexed_header(read_csv_header(data_file_path, delimiter)),
+        create_indexed_header(read_csv_header(data, delimiter)),
         item_header_name,
         delimiter
     )
     dataset = ItemSessionDataset(reader, session_parser)
-    print(len(dataset))
     builder = NextItemIndexBuilder(min_session_length)
-    builder.build(dataset, output_file_path)
+    builder.build(dataset, output)
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("data_file", help="path to the csv data file", type=str)
-    parser.add_argument("session_index_file", help="path to the session index file", type=str)
-    parser.add_argument("output_file", help="path where the index is written to", type=str)
-    parser.add_argument("item_header_name", help="name of the item column (header)", type=str)
-    parser.add_argument("--min_session_length", help="minimum session length (Default: 2)", default=2, type=int)
-    parser.add_argument("--delimiter", help="delimiter used in the csv data file (Default: \\t).", default="\t", type=str)
-
-    args = parser.parse_args()
-
-    data_file = Path(args.data_file)
-    session_index_file = Path(args.session_index_file)
-    output_file = Path(args.output_file)
-    item_header_name = args.item_header_name
-
-    min_session_length = args.min_session_length
-    delimiter = args.delimiter
-
-    run(data_file, session_index_file, output_file, item_header_name, min_session_length, delimiter)
+    app()
