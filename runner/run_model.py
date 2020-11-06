121cb17ffd1ca967ad1b4ea84e0e964ee7d8675a
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2020-11-06 10:55:18 +0100

    switched to typer instead of argparse

--- runner/run_model.py
+++ runner/run_model.py
@@ -1,9 +1,12 @@
 import os
-from argparse import ArgumentParser
 
+import typer
 from dependency_injector import containers
 
 from runner.util.containers import BERT4RecContainer, CaserContainer, SASRecContainer, NarmContainer, GRUContainer
+
+
+app = typer.Typer()
 
 
 # TODO: introduce a subclass for all container configurations?
@@ -17,29 +20,23 @@ def build_container(model_id) -> containers.DeclarativeContainer:
     }[model_id]
 
 
-def run_model(model_id: str,
-              path_to_config_file: str
+@app.command()
+def run_model(model: str = typer.Argument(..., help="the model to run"),
+              config_file: str = typer.Argument(..., help='the path to the config file')
               ) -> None:
-    container = build_container(model_id)
-    container.config.from_yaml(path_to_config_file)
+    # XXX: because the dependency injector does not provide a error message when the config file does not exists,
+    # we manually check if the config file exists
+    if not os.path.isfile(config_file):
+        print(f"the config file cannot be found. Please check the path '{config_file}'!")
+        exit(-1)
+
+    container = build_container(model)
+    container.config.from_yaml(config_file)
     module = container.module()
 
     trainer = container.trainer()
     trainer.fit(module, train_dataloader=container.train_loader(), val_dataloaders=container.validation_loader())
 
 
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument("model", help="the model to train", type=str)
-    parser.add_argument("path_to_config_file", help="the path to the config file", type=str)
-
-    args = parser.parse_args()
-
-    # XXX: because the dependency injector does not provide a error message when the config file does not exists,
-    # we manually check if the config file exists
-    config_file = args.path_to_config_file
-    if not os.path.isfile(config_file):
-        print(f"the config file cannot be found. Please check the path '{config_file}'!")
-        exit(-1)
-
-    run_model(args.model, config_file)
+if __name__ == "__main__":
+    app()
