1726950c0d0fdd558a55c6ec16148dfac00f50da
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2020-11-08 17:23:26 +0100

    added gru pooler for more than one layer grus

--- models/gru/gru_model.py
+++ models/gru/gru_model.py
@@ -2,6 +2,25 @@ import torch
 import torch.nn as nn
 
 
+class GRUPooler(nn.Module):
+
+    def __init__(self,
+                 hidden_size: int,
+                 num_items: int
+                 ):
+        super().__init__()
+
+        self.fcn = nn.Linear(hidden_size, num_items, bias=True)
+
+    def forward(self,
+                outputs: torch.Tensor,
+                hidden_representation: torch.Tensor
+                ) -> torch.Tensor:
+        # we "pool" the model by simply taking the hidden state of the last layer
+        representation = hidden_representation[-1, :, :]
+        return self.fcn(representation)
+
+
 class GRUSeqItemRecommenderModel(nn.Module):
 
     def __init__(self,
@@ -20,7 +39,8 @@ class GRUSeqItemRecommenderModel(nn.Module):
             dropout = 0
 
         self.gru = nn.GRU(item_embedding_dim, hidden_size, dropout=dropout, num_layers=num_layers, batch_first=True)
-        self.fcn = nn.Linear(hidden_size, num_items, bias=True)
+        self.pooling = GRUPooler(hidden_size, num_items)
+
         self.dropout = nn.Dropout2d(p=dropout)
 
     def forward(self, session, mask: torch.Tensor, batch_idx):
@@ -32,7 +52,6 @@ class GRUSeqItemRecommenderModel(nn.Module):
             batch_first=True,
             enforce_sorted=False
         )
-        _, final_state = self.gru(packed_embedded_session)
-
-        output = self.fcn(final_state)
-        return torch.squeeze(output, dim=0)
+        outputs, final_state = self.gru(packed_embedded_session)
+        output = self.pooling(outputs, final_state)
+        return output
