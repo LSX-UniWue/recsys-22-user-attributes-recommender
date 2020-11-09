a80e9747fdc925cc421c34d40cc5285a68e6ce7e
Alexander Dallmann <dallman@informatik.uni-wuerzburg.de>
2020-11-09 21:00:21 +0100

    initial container

--- k8s/README.md
+++ k8s/README.md
@@ -14,3 +14,8 @@ purpose.
 2. Give it a name, e.g. `k8s`
 3. Select `read_repository`
 4. Save the final token, because it won't be accessible afterwards
+
+## Run
+```
+podman run -e GIT_TOKEN=S369mz5Ur1hS2KFBxiDH -e REPO_USER=ald58ab -e REPO_BRANCH=container --mount=type=bind,source=/home/dallmann/uni/research/dota/datasets/small/preprocessed/match_split,dst=/dataset --mount=type=bind,source=/home/dallmann/tmp/configs,dst=/configs recommender:latest poetry run python -m runner.run_model narm /configs/narm_config.yaml
+```
--- k8s/entrypoint.sh
+++ k8s/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 REPO_URL="https://${REPO_USER}@gitlab2.informatik.uni-wuerzburg.de/dmir/dallmann/recommender.git"
+#DATA_REMOTE_PATH=/home/ls6/dallmann/datasets <- put the path in ceph that you need to sync to the ssd into this env variable
+#DATA_LOCAL_PATH=/cache <- set this env variable to the ssd cache path
 mkdir -p "${PROJECT_DIR}"
 cd "${PROJECT_DIR}"
 git clone -q "${REPO_URL}" code
 cd code
 git checkout -q "${REPO_BRANCH}"
-poetry shell
+poetry install
+cp -r $DATA_REMOTE_PATH $DATA_LOCAL_PATH
 /bin/bash -c "$*"
