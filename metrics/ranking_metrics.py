1ad6157789be9b4202c4ea44637f933138024190
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2020-11-20 10:25:58 +0100

    fixing mrr for single item recommendation

--- metrics/ranking_metrics.py
+++ metrics/ranking_metrics.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 import pytorch_lightning as pl
 import torch
 
@@ -52,7 +54,25 @@ def calc_recall(prediction: torch.Tensor,
     return tp / (tp + fn)
 
 
-class F1AtMetric(pl.metrics.Metric):
+class RecommendationMetric(pl.metrics.Metric):
+
+    def update(self,
+               predictions: torch.Tensor,
+               target: torch.Tensor,
+               mask: torch.Tensor
+               ) -> None:
+        self._update(predictions, target, mask)
+
+    @abstractmethod
+    def _update(self,
+                predictions: torch.Tensor,
+                target: torch.Tensor,
+                mask: torch.Tensor
+                ) -> None:
+        pass
+
+
+class F1AtMetric(RecommendationMetric):
 
     def __init__(self,
                  k: int,
@@ -63,11 +83,11 @@ class F1AtMetric(pl.metrics.Metric):
         self.add_state("count", torch.tensor(0), dist_reduce_fx="sum")
         self._k = k
 
-    def update(self,
-               prediction: torch.Tensor,
-               target: torch.Tensor,
-               mask: torch.Tensor = None,
-               ):
+    def _update(self,
+                prediction: torch.Tensor,
+                target: torch.Tensor,
+                mask: torch.Tensor = None,
+                ) -> None:
         recall = calc_recall(prediction, target, self._k, mask=mask)
         precision = calc_precision(prediction, target, self._k, mask=mask)
 
@@ -81,7 +101,7 @@ class F1AtMetric(pl.metrics.Metric):
         return self.f1 / self.count
 
 
-class PrecisionAtMetric(pl.metrics.Metric):
+class PrecisionAtMetric(RecommendationMetric):
 
     def __init__(self,
                  k: int,
@@ -92,11 +112,11 @@ class PrecisionAtMetric(pl.metrics.Metric):
         self.add_state("precision", torch.tensor(0.0), dist_reduce_fx="sum")
         self.add_state("count", torch.tensor(0), dist_reduce_fx="sum")
 
-    def update(self,
-               prediction: torch.Tensor,
-               target: torch.Tensor,
-               mask: torch.Tensor = None,
-               ):
+    def _update(self,
+                prediction: torch.Tensor,
+                target: torch.Tensor,
+                mask: torch.Tensor = None,
+                ) -> None:
         """
         :param prediction: the scores of all items :math `(N, I)`
         :param target: the target label tensor :math `(N, T)` or :math `(N)`
@@ -111,7 +131,7 @@ class PrecisionAtMetric(pl.metrics.Metric):
         return self.precision / self.count
 
 
-class RecallAtMetric(pl.metrics.Metric):
+class RecallAtMetric(RecommendationMetric):
     def __init__(self,
                  k: int,
                  dist_sync_on_step: bool = False):
@@ -121,12 +141,11 @@ class RecallAtMetric(pl.metrics.Metric):
         self.add_state("recall", torch.tensor(0.), dist_reduce_fx="sum")
         self.add_state("count", torch.tensor(0), dist_reduce_fx="sum")
 
-
-    def update(self,
-               prediction: torch.Tensor,
-               target: torch.Tensor,
-               mask: torch.Tensor = None,
-               ):
+    def _update(self,
+                prediction: torch.Tensor,
+                target: torch.Tensor,
+                mask: torch.Tensor = None,
+                ) -> None:
         """
         :param prediction: the scores of all items :math `(N, I)`
         :param target: the target label tensor :math `(N, T)` or :math `(N)`
@@ -142,7 +161,7 @@ class RecallAtMetric(pl.metrics.Metric):
         return self.recall / self.count
 
 
-class MRRAtMetric(pl.metrics.Metric):
+class MRRAtMetric(RecommendationMetric):
 
     def __init__(self,
                  k: int,
@@ -154,11 +173,11 @@ class MRRAtMetric(pl.metrics.Metric):
         self.add_state("mrr", torch.tensor(0.), dist_reduce_fx="sum")
         self.add_state("count", torch.tensor(0), dist_reduce_fx="sum")
 
-
-    def update(self,
-               prediction: torch.Tensor,
-               target: torch.Tensor
-               ) -> torch.Tensor:
+    def _update(self,
+                prediction: torch.Tensor,
+                target: torch.Tensor,
+                mask: torch.Tensor
+                ) -> None:
         """
         :param prediction: the scores of all items, the first one is the positive item, all others are negatives :math `(N, I)`
         :param target: the target label tensor :math `(N, T)`
@@ -178,43 +197,3 @@ class MRRAtMetric(pl.metrics.Metric):
 
     def compute(self):
         return self.mrr / self.count
-
-
-if __name__ == "__main__":
-    test_prediction = torch.tensor([
-        [0.5, 0.12, 0.3, 0.18],  # ranking: 0, 2, 3, 1
-        [0.5, 0.12, 0.6, 0.18],  # ranking: 2, 0, 3, 1
-    ])
-    test_target = torch.tensor([[0], [12]])  # assume arbitrary masking id
-    #mask = torch.tensor([[1, 1], [1, 1]])
-    # k=3
-    # max_target_size = 2
-    #
-    # if max_target_size > k:
-    #     raise Exception()
-    #
-    # sorted_indices = torch.topk(prediction, k=k)[1]
-    # sorted_indices = torch.repeat_interleave(sorted_indices, max_target_size, dim=0)
-    #
-    #
-    # print(sorted_indices)
-    #
-    # target = torch.tensor([[2, 1], [0, 3]])  # assume arbitrary masking id
-    # mask = torch.tensor([[1, 1], [1, 0]])  # mask marks last entry in target as padded
-    #
-    #
-    # target = target.view(-1, 1)
-    # print(target)
-    # target = target.expand(-1, k)
-    # print(target)
-    #
-    # rank = torch.topk((sorted_indices.eq(target) * torch.arange(1, k + 1, dtype=torch.float, device=target.device)), k=k)[0]
-    # print(rank)
-    #
-    # rank = rank * mask.view([1, -1])
-    # print(rank)
-
-    metric = RecallAtMetric(k=1)
-    metric(test_prediction, test_target)
-    print(metric.compute())
-
