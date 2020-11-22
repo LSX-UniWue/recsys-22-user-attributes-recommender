cade11efb1ddf450cab46701036989f7f44f931f
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2020-11-22 17:34:28 +0100

    cleanup
    started to fix the movielens helper app

--- dataset/movielens.py
+++ dataset/movielens.py
@@ -1,10 +1,21 @@
-from dataset.utils import *
+from pathlib import Path
+
+from dataset.utils import maybe_download, unzip_file
 import pandas as pd
 import typer
 
 app = typer.Typer()
 
-def preprocess_data(dataset_dir, name):
+
+DOWNLOAD_URL_MAP = {
+    'ml-1m': 'http://files.grouplens.org/datasets/movielens/ml-1m.zip',
+    'ml-20m': 'http://files.grouplens.org/datasets/movielens/ml-20m.zip'
+}
+
+
+def preprocess_data(dataset_dir: Path,
+                    name: str
+                    ):
     """
     Convert raw movielens data to csv files and create vocabularies
     :param dataset_dir:
@@ -22,7 +33,7 @@ def preprocess_data(dataset_dir, name):
         header = 0
         sep = ","
 
-    #Read and merge data
+    # read and merge data
     ratings_df = read_csv(dataset_dir, "ratings", file_type, sep, header)
     movies_df = read_csv(dataset_dir, "movies", file_type, sep, header)
 
@@ -30,7 +41,7 @@ def preprocess_data(dataset_dir, name):
         ratings_df.columns = ['userId', 'movieId', 'rating', 'timestamp']
         movies_df.columns = ['movieId', 'title', 'genres']
         users_df = read_csv(dataset_dir, "users", file_type, sep, header)
-        users_df.columns = ['userId', 'gender', 'age','occupation','zip']
+        users_df.columns = ['userId', 'gender', 'age', 'occupation', 'zip']
         ratings_df = pd.merge(ratings_df, users_df)
 
     elif name == "ml-20":
@@ -38,9 +49,9 @@ def preprocess_data(dataset_dir, name):
         ratings_df = pd.merge(ratings_df, links_df)
 
     merged_df = pd.merge(ratings_df, movies_df).sort_values(by=["userId", "timestamp"])
-    merged_df.to_csv("/".join([dataset_dir, name + ".csv"]), sep="\t", index=False)
+    merged_df.to_csv(dataset_dir / f"{name}.csv", sep="\t", index=False)
 
-    #build vocabularies
+    # build vocabularies
     build_vocabularies(movies_df, dataset_dir, "title")
     build_vocabularies(movies_df, dataset_dir, "genres", split="|")
     build_vocabularies(users_df, dataset_dir, "gender")
@@ -49,11 +60,15 @@ def preprocess_data(dataset_dir, name):
     build_vocabularies(users_df, dataset_dir, "zip")
 
 
-def build_vocabularies(dataframe, dataset_dir, column, split = ""):
+def build_vocabularies(dataframe: pd.DataFrame,
+                       dataset_dir: Path,
+                       column: str,
+                       split: str = ""
+                       ) -> None:
     """
     Build and write a vocabulary file
     :param dataframe: base dataframe
-    :param dataset_folder: folder for saving file
+    :param dataset_dir: folder for saving file
     :param dataset_name: dataset name for saving file
     :param column: column to create vocabulary for
     :param split: token to split if column need splitting
@@ -65,52 +80,36 @@ def build_vocabularies(dataframe, dataset_dir, column, split = ""):
         dataframe.columns = ['index', column]
 
     title_vocab = pd.DataFrame(dataframe[column].unique())
-    special_tokens = pd.DataFrame(["<PAD>","<MASK>","<UNK>"])
+    special_tokens = pd.DataFrame(["<PAD>", "<MASK>", "<UNK>"])
     title_vocab = title_vocab.append(special_tokens).reset_index(drop=True)
     title_vocab["id"] = title_vocab.index
-    os.makedirs("/".join([dataset_dir, "vocab"]), exist_ok= True)
-    title_vocab.to_csv(("/".join([dataset_dir, "vocab", column + ".vocab"])), index=False, sep="\t")
+
+    vocab_file = dataset_dir / f'vocab_{column}.txt'
+    title_vocab.to_csv(vocab_file, index=False, sep="\t")
 
 
-def read_csv(dataset_dir, file, file_type, sep, header=None):
-    file_path = "/".join([dataset_dir, file+file_type])
+def read_csv(dataset_dir: Path,
+             file: str,
+             file_type: str,
+             sep: str,
+             header: bool = None
+             ) -> pd.DataFrame:
+    file_path = dataset_dir / f"{file}{file_type}"
     return pd.read_csv(file_path, sep=sep, header=header, engine="python")
 
-@app.command()
-def main(dataset: str = typer.Argument(..., help="ml-1m or ml-20m"),
-         dir: str = typer.Option("./dataset/",help='directory to save data')):
-
-    if dataset == "ml-1m":
-        url = "http://files.grouplens.org/datasets/movielens/ml-1m.zip"
-    elif dataset == "ml-20m":
-        url = "http://files.grouplens.org/datasets/movielens/ml-20m.zip"
-    else:
-        print("Invalid dataset name")
-        exit()
-
-    dataset_dir = dir + "/" +dataset
-
-    file = maybe_download(url, dataset_dir)
-    unzip_file(file, dir, True)
-    preprocess_data(dataset_dir, dataset)
 
 @app.command()
-def main(dataset: str = typer.Argument(..., help="ml-1m or ml-20m"),
-         dir: str = typer.Option("./dataset/",help='directory to save data')):
+def main(dataset: str = typer.Argument(..., help="ml-1m or ml-20m", show_choices=True),
+         output_dir: Path = typer.Option("./dataset/", help='directory to save data')
+         ) -> None:
 
-    if dataset == "ml-1m":
-        url = "http://files.grouplens.org/datasets/movielens/ml-1m.zip"
-    elif dataset == "ml-20m":
-        url = "http://files.grouplens.org/datasets/movielens/ml-20m.zip"
-    else:
-        print("Invalid dataset name")
-        exit()
-
-    dataset_dir = dir + "/" +dataset
+    url = DOWNLOAD_URL_MAP[dataset]
+    dataset_dir = output_dir / dataset
 
     file = maybe_download(url, dataset_dir)
-    unzip_file(file, dir, True)
+    unzip_file(file, output_dir, delete=False)
     preprocess_data(dataset_dir, dataset)
+
 
 if __name__ == "__main__":
     app()
