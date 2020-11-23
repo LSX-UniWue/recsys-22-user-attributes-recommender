18a6e3f68e28533cd9bd34d099f650a718e3345e
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2020-11-23 15:22:34 +0100

    allowing entrypoint to have no prepare script

--- k8s/entrypoint.sh
+++ k8s/entrypoint.sh
@@ -4,14 +4,23 @@ REPO_URL="https://${REPO_USER}@gitlab2.informatik.uni-wuerzburg.de/dmir/dallmann
 #DATA_LOCAL_PATH=/cache <- set this env variable to the ssd cache path
 #PREPARE_SCRIPT=somewhere
 #RUN_SCRIPT
+# create project dir
 mkdir -p "${PROJECT_DIR}"
 cd "${PROJECT_DIR}"
+# checkout the repository
 git clone -q "${REPO_URL}"
-cd recommender
+cd recommender || exit 1
 git checkout -q "${REPO_BRANCH}"
-which poetry
+
+# install requirements using poetry
 ls -lh `which poetry`
 poetry install
-chmod +x $PREPARE_SCRIPT
-$PREPARE_SCRIPT
+
+# execute the provided prepare script
+if [ ! -z "$PREPARE_SCRIPT" ]; then
+  chmod +x "${PREPARE_SCRIPT}"
+  $PREPARE_SCRIPT
+fi
+
+# now run the configuration using poetry
 /bin/bash -c "cd ${PROJECT_DIR}/recommender && poetry run python -m runner.run_model $*"
