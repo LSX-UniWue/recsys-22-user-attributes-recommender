91f87dc214dcc3316f3e1e8fe6d2ae31a4926e42
Johannes Kohlmann <johannes.kohlmann@stud-mail.uni-wuerzburg.de>
2020-11-24 19:05:04 +0100

    Updated metric logger to use the new return dicts.

--- logger/MetricLoggerCallback.py
+++ logger/MetricLoggerCallback.py
@@ -2,6 +2,7 @@ from pytorch_lightning import Callback
 from pytorch_lightning.core import LightningModule
 
 from data.datasets import TARGET_ENTRY_NAME
+from modules.constants import RETURN_KEY_PREDICTIONS, RETURN_KEY_TARGETS, RETURN_KEY_MASK
 
 
 def check_has_metrics(module: LightningModule):
@@ -11,9 +12,11 @@ class MetricLoggerCallback(Callback):
 
     def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
         if check_has_metrics(pl_module):
-            targets = batch[TARGET_ENTRY_NAME]
+            targets = outputs[RETURN_KEY_TARGETS]
+            predictions = outputs[RETURN_KEY_PREDICTIONS]
+            mask = outputs[RETURN_KEY_MASK]
             for name, metric in pl_module.metrics.items():
-                step_value = metric(outputs, targets)
+                step_value = metric(predictions, targets, mask=mask)
                 pl_module.log(name, step_value, prog_bar=True)
 
     def on_validation_epoch_end(self, trainer, pl_module):
