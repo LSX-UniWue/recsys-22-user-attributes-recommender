d29e5c184e0b44a3c76a698d84f660a5f0e369fc
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2020-11-27 11:42:33 +0100

    movielens uses now the min session length

--- dataset/utils.py
+++ dataset/utils.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from pathlib import Path
 
 import requests
@@ -6,18 +7,18 @@ from tqdm import tqdm
 import zipfile
 
 
-def maybe_download(url: str,
-                   directory: Path
-                   ) -> Path:
+def download_dataset(url: str,
+                     download_dir: Path
+                     ) -> Path:
     """
     Downloads file if it doesn't already exists
     :param url:
-    :param directory:
+    :param download_dir: the download dir
     :return:
     """
     filename = url.split("/")[-1]
-    os.makedirs(directory, exist_ok=True)
-    filepath = directory / filename
+    os.makedirs(download_dir, exist_ok=True)
+    filepath = download_dir / filename
     if not os.path.exists(filepath):
         response = requests.get(url, stream=True)
         total_size_in_bytes = int(response.headers.get('content-length', 0))
@@ -44,10 +45,31 @@ def unzip_file(src_file: Path,
             folder (Path): Destination folder.
             delete (bool): Whether or not to delete the zip file after unzipping.
         """
+    files_already_extracted = os.listdir(folder)
+    if len(files_already_extracted) > 0:
+        return
+
     fz = zipfile.ZipFile(src_file, "r")
 
+    root_paths = set()
+    files_extracted = []
+
     for file in tqdm(iterable=fz.namelist(), total=len(fz.namelist())):
-        fz.extract(file, folder)
+        fz.extract(file, str(folder))
+        files_extracted.append(file)
+
+        file_path = Path(file)
+        root_path = file_path.parent
+        if root_path != Path('.'):
+            root_paths.add(root_path)
+
+    if len(root_paths) == 1:
+        root_path = folder / next(iter(root_paths))
+        files = os.listdir(root_path)
+        for file in files:
+            shutil.move(str(root_path / file), str(folder))
+
+        shutil.rmtree(str(root_path))
 
     if delete:
         os.remove(src_file)
