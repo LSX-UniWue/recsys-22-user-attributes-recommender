fixed fixed item sampler for basket setting added f1, precision and ndcg

metrics/container/metrics_sampler.py
@@ -52,11 +52,21 @@ class FixedItemsSampler(MetricsSampler):
                predictions: torch.Tensor,
                mask: Optional[torch.Tensor] = None) -> MetricsSample:
         fixed_items = torch.LongTensor(self.fixed_items).to(targets.device)
-        fixed_items = fixed_items.unsqueeze(0).repeat(predictions.size()[0], 1)
+        fixed_items = fixed_items.unsqueeze(0).repeat(targets.size()[0], 1)
 
         sampled_predictions = predictions.gather(1, fixed_items)
-        target_batched = targets.unsqueeze(1)
-        positive_item_mask = fixed_items.eq(target_batched).to(dtype=predictions.dtype)
+
+        if len(targets.size()) == 1:
+            # next-item recommendation
+            target_batched = targets.unsqueeze(1)
+            positive_item_mask = fixed_items.eq(target_batched).to(dtype=predictions.dtype)
+        else:
+            # here we multi-hot encode the targets (1 for each target in the item space)
+            multihot = torch.zeros_like(predictions).to(torch.long)
+            src = torch.ones_like(multihot).to(torch.long)
+            multihot.scatter_(1, targets, src)
+            # and than gather the corresponding indices by the fixed items
+            positive_item_mask = multihot.gather(1, fixed_items)
 
         return MetricsSample(sampled_predictions, positive_item_mask)
 

metrics/sampling/ndcg.py
@@ -1,10 +1,10 @@
 import torch
 
-from metrics.sampling.common import calc_recall
+from metrics.sampling.common import calc_ndcg
 from metrics.sampling.sampling_metric import SamplingMetric
 
 
-class RecallAtNegativeSamples(SamplingMetric):
+class NDCGAtNegativeSamples(SamplingMetric):
 
     def __init__(self,
                  k: int,
@@ -12,7 +12,7 @@ class RecallAtNegativeSamples(SamplingMetric):
         super().__init__(dist_sync_on_step=dist_sync_on_step)
         self._k = k
 
-        self.add_state("recall", torch.tensor(0.), dist_reduce_fx="sum")
+        self.add_state("ndcg", torch.tensor(0.), dist_reduce_fx="sum")
         self.add_state('count', torch.tensor(0.), dist_reduce_fx="sum")
 
     def _update(self,
@@ -25,12 +25,12 @@ class RecallAtNegativeSamples(SamplingMetric):
         :param positive_item_mask: a mask where a 1 indices that the item at this index is relevant :math`(N, I)`
         :return:
         """
-        recall = calc_recall(prediction, positive_item_mask, self._k)
-        self.recall += recall.sum()
+        ndcg = calc_ndcg(prediction, positive_item_mask, self._k)
+        self.ndcg += ndcg.sum()
         self.count += prediction.size()[0]
 
     def compute(self):
-        return self.recall / self.count
+        return self.ndcg / self.count
 
     def name(self):
-        return f"recall_at_{self._k}/sampled"
+        return f"ndcg_at_{self._k}/sampled"