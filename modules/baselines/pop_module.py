f8f021f1f7f944e6f0366b2947de267c598b5ec2
Johannes Kohlmann <johannes.kohlmann@stud-mail.uni-wuerzburg.de>
2021-02-22 17:10:13 +0100

    Warning when training with max_epochs > 1 instead of force setting it to 1.

--- modules/baselines/pop_module.py
+++ modules/baselines/pop_module.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 import torch
+from pytorch_lightning.utilities import rank_zero_warn
 from torch.nn.parameter import Parameter
 
 from data.datasets import ITEM_SEQ_ENTRY_NAME, TARGET_ENTRY_NAME
@@ -25,11 +26,14 @@ class PopModule(MetricsTrait, pl.LightningModule):
         self.tokenizer = tokenizer
         self.metrics = metrics
 
-        self.popularities = Parameter(torch.zeros(self.item_vocab_size, device=self.device),requires_grad=False)
+        self.popularities = Parameter(torch.zeros(self.item_vocab_size, device=self.device), requires_grad=False)
 
     def on_train_start(self) -> None:
         # TODO: This is a really hacky way to end training after a single epoch, can we do better?
-        self.trainer.max_epochs = 1
+        if self.trainer.max_epochs > 1:
+            rank_zero_warn(
+                f"When training the POP baseline, "
+                f"'trainer.max_epochs' should be set to 1 (but was {self.trainer.max_epochs}).")
 
     def get_metrics(self) -> MetricsContainer:
         return self.metrics
@@ -41,7 +45,7 @@ class PopModule(MetricsTrait, pl.LightningModule):
         predictions[:, pop] = 1
         return predictions
 
-    def training_step(self, batch: Dict[str,torch.tensor], batch_idx):
+    def training_step(self, batch: Dict[str, torch.tensor], batch_idx):
         input_seq = batch[ITEM_SEQ_ENTRY_NAME]
         targets = batch[TARGET_ENTRY_NAME]
 
@@ -49,12 +53,12 @@ class PopModule(MetricsTrait, pl.LightningModule):
             targets = targets.unsqueeze(dim=1)
 
         mask = get_padding_mask(input_seq, self.tokenizer)
-        full_sequence = torch.cat([input_seq * mask, targets],dim=1)
+        full_sequence = torch.cat([input_seq * mask, targets], dim=1)
         self.popularities += torch.bincount(full_sequence[full_sequence > 0].flatten(), minlength=self.item_vocab_size)
 
         return {'loss': torch.tensor(0., device=self.device)}
 
-    def eval_step(self, batch: Dict[str,torch.tensor], batch_idx):
+    def eval_step(self, batch: Dict[str, torch.tensor], batch_idx):
         input_seq = batch[ITEM_SEQ_ENTRY_NAME]
         targets = batch[TARGET_ENTRY_NAME]
         prediction = self.forward(input_seq)
@@ -67,8 +71,9 @@ class PopModule(MetricsTrait, pl.LightningModule):
     def test_step(self, batch: Dict[str, torch.tensor], batch_idx):
         return self.eval_step(batch, batch_idx)
 
-    # Do nothing on backward since we only count occurences
-    def backward(self, loss: torch.Tensor, optimizer: torch.optim.Optimizer, optimizer_idx: int, *args, **kwargs) -> None:
+    # Do nothing on backward since we only count occurrences
+    def backward(self, loss: torch.Tensor, optimizer: torch.optim.Optimizer, optimizer_idx: int, *args,
+                 **kwargs) -> None:
         pass
 
     def configure_optimizers(self):
