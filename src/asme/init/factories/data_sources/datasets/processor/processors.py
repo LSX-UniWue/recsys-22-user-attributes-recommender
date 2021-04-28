8210996e956cdfa431bdaca355bff3a6e7904c4b
Elisabeth Fischer <elisabeth.fischer@informatik.uni-wuerzburg.de>
2021-04-28 13:34:34 +0200

    Add alternative target extractor for prediction of the whole sequence

--- src/asme/init/factories/data_sources/datasets/processor/processors.py
+++ src/asme/init/factories/data_sources/datasets/processor/processors.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from asme.init.factories.data_sources.datasets.processor.target_extractor import TargetExtractorProcessorFactory
+from asme.init.factories.data_sources.datasets.processor.no_target_extractor import NoTargetExtractorProcessorFactory
 from data.datasets.processors.processor import Processor
 from asme.init.config import Config
 from asme.init.context import Context
@@ -31,7 +32,8 @@ class ProcessorsFactory(ObjectFactory):
                 'last_item_mask': LastItemMaskProcessorFactory(),
                 'position_token': PositionTokenProcessorFactory(),
                 'tokenizer': TokenizerProcessorFactory(),
-                'target_extractor': TargetExtractorProcessorFactory()
+                'target_extractor': TargetExtractorProcessorFactory(),
+                'no_target_extractor': NoTargetExtractorProcessorFactory()
             })
         )
 
