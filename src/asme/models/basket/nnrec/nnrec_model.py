4e640c69b39ecadf10dfb55a448070e774b606d9
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2021-05-27 13:55:19 +0200

    refactored nnrec model

--- src/asme/models/basket/nnrec/nnrec_model.py
+++ src/asme/models/basket/nnrec/nnrec_model.py
@@ -1,12 +1,28 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 import torch
-from asme.models.sequence_recommendation_model import SequenceRecommenderModel
+from asme.models.sequence_recommendation_model import SequenceRecommenderModel, SequenceRepresentationLayer, IdentitySequenceRepresentationModifierLayer
 from data.datasets import USER_ENTRY_NAME
 from torch import nn
 
-from asme.models.layers.layers import ItemEmbedding
+from asme.models.layers.layers import ItemEmbedding, LinearProjectionLayer
 from asme.utils.hyperparameter_utils import save_hyperparameters
+
+
+class NNRecSequenceRepresentationLayer(SequenceRepresentationLayer):
+
+    def __init__(self,
+                 embedding_size: int,
+                 hidden_size: int):
+        self.hidden_layer = nn.Linear(embedding_size, hidden_size)
+        self.act1 = nn.Tanh()
+
+    def forward(self,
+                sequence: torch.Tensor,
+                padding_mask: Optional[torch.Tensor] = None,
+                **kwargs: Dict[str, torch.Tensor]
+                ) -> torch.Tensor:
+        return self.act1(self.hidden_layer(sequence))
 
 
 class NNRecModel(SequenceRecommenderModel):
@@ -27,48 +43,27 @@ class NNRecModel(SequenceRecommenderModel):
                  max_sequence_length: int,  # k, number of last baskets in the paper
                  embedding_pooling_type: str
                  ):
-        super().__init__()
+        # layer 1 in the paper
+        seq_rep_layer = NNRecSequenceRepresentationLayer(max_sequence_length * item_embedding_size + user_embedding_size, hidden_size)
+
+        projection_layer = LinearProjectionLayer(hidden_size, item_vocab_size) # layer 2 in the paper
+        super().__init__(None, seq_rep_layer, IdentitySequenceRepresentationModifierLayer(), projection_layer)
 
         self.item_embedding = ItemEmbedding(item_vocab_size, item_embedding_size,
                                             embedding_pooling_type=embedding_pooling_type)
+        # batch_size = embedded_items.size()[0]
+        # embedded_items = embedded_items.view(batch_size, -1)
+
+        # if user is not None:
+        #    embedded_user = self.user_embedding(user)
+        #    overall_representation = torch.cat([embedded_user, embedded_items])
+        # else:
+        #    overall_representation = embedded_items
 
         # if we have no user the embedding can be ignored
         self.user_embedding_size = user_embedding_size
         if user_embedding_size > 0:
             self.user_embedding = nn.Embedding(user_vocab_size, user_embedding_size)
-
-        # layer 1 in the paper
-        self.hidden_layer = nn.Linear(max_sequence_length * item_embedding_size + user_embedding_size, hidden_size)
-        self.act1 = nn.Tanh()
-
-        self.projection_layer = nn.Linear(hidden_size, item_vocab_size)  # layer 2 in the paper
-
-    def forward(self,
-                input_sequence: torch.Tensor,
-                padding_mask: Optional[torch.Tensor] = None,
-                user: Optional[torch.Tensor] = None
-                ) -> torch.Tensor:
-        """
-        performs a forward path
-        :param input_sequence: input sequence :math`(N, S)`
-        :param padding_mask: not used for this model
-        :param user: the user id :math`(N)`
-        :return: the logits for each item :math`(N, I)`
-        """
-
-        embedded_items = self.item_embedding(input_sequence)
-        batch_size = embedded_items.size()[0]
-        embedded_items = embedded_items.view(batch_size, -1)
-
-        if user is not None:
-            embedded_user = self.user_embedding(user)
-            overall_representation = torch.cat([embedded_user, embedded_items])
-        else:
-            overall_representation = embedded_items
-
-        first_hidden = self.act1(self.hidden_layer(overall_representation))
-
-        return self.projection_layer(first_hidden)
 
     def optional_metadata_keys(self) -> List[str]:
         return [USER_ENTRY_NAME]
