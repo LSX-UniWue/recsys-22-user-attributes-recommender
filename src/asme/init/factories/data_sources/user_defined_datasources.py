f1e213fdf6adf73f12b5d50971fb12c10d64c898
Johannes Kohlmann <johannes.kohlmann@stud-mail.uni-wuerzburg.de>
2021-06-29 17:11:20 +0200

    Added some documentation

--- src/asme/init/factories/data_sources/user_defined_datasources.py
+++ src/asme/init/factories/data_sources/user_defined_datasources.py
@@ -3,7 +3,6 @@ from typing import List, Union, Any, Dict
 
 from asme.init.config import Config
 from asme.init.context import Context
-from asme.init.factories.common.conditional_based_factory import ConditionalFactory
 from asme.init.factories.data_sources.common import build_default_loader_config
 from asme.init.factories.data_sources.loader import LoaderFactory
 from asme.init.object_factory import ObjectFactory, CanBuildResult, CanBuildResultType
@@ -42,12 +41,14 @@ class UserDefinedDataSourcesFactory(ObjectFactory):
         objects = {}
         split = DatasetSplit[config.get("split").upper()]
         for stage in Stage:
+            # Use default values from the configuration and override them with values provided for the specific stage
             stage_config = _build_stage_config(config, stage)
             dataset_builder = self.DATASET_BUILDER[split][stage_config.pop("type")]
-            processor_config = [TARGET_EXTRACTOR_PROCESSOR_CONFIG] if not stage_config.has_path("processors") \
-                else stage_config.pop("processors")
-            loader_config = build_default_loader_config(stage_config, stage, [dataset_builder], processor_config)
+            # When using user defined data sources, all processors except the tokenizer have to specified explicitly
+            processor_config = stage_config.pop("processors")
 
+            # Generate the loader config similar to the template data sources
+            loader_config = build_default_loader_config(stage_config, stage, [dataset_builder], processor_config)
             objects[stage.value] = self._factory.build(loader_config, context)
 
         return objects
@@ -63,6 +64,16 @@ class UserDefinedDataSourcesFactory(ObjectFactory):
 
 
 def _build_stage_config(config: Config, stage: Stage) -> Config:
+    """
+    Generates the config for a specific stage by merging the values of the parent config with those of the stage
+    specific one. If a key is present in both configs, the value of the stage specific config takes precedence.
+
+    :param config: The config including stage specific data as well as general information that might be used to
+                   substitute values missing from the stage specific one.
+    :param stage: The stage the config should be built for.
+
+    :return: The stage specific configuration.
+    """
     overwritten_stage_values = config.get_config([stage.value])
     default_values = _exclude_keys(config, [s.value for s in Stage])
     for key in overwritten_stage_values.get_keys():
@@ -71,8 +82,16 @@ def _build_stage_config(config: Config, stage: Stage) -> Config:
     return default_values
 
 
-def _exclude_keys(config: Config, key_to_exclude: List[str]) -> Config:
+def _exclude_keys(config: Config, keys_to_exclude: List[str]) -> Config:
+    """
+    Deepcopies the config but excludes the specified keys.
+
+    :param config: The input config to copy.
+    :param keys_to_exclude: The keys to exclude in the new config.
+
+    :return: A config containing all original keys but the specified ones.
+    """
     new_config = copy.deepcopy(config)
-    for key in key_to_exclude:
+    for key in keys_to_exclude:
         new_config.pop(key)
     return new_config
