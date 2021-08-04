255f23fe6b2eb32bd8fe5f7b4b0011fc04275288
Johannes Kohlmann <johannes.kohlmann@stud-mail.uni-wuerzburg.de>
2021-08-04 16:08:08 +0200

    Not dropping movieId in the ml-20m converter since we need it later.

--- src/data/datamodule/converters.py
+++ src/data/datamodule/converters.py
@@ -71,7 +71,7 @@ class Movielens20MConverter(CsvConverter):
 
     def apply(self, input_dir: Path, output_file: Path):
         file_type = ".csv"
-        header = None
+        header = 0
         sep = ","
         name = "ml-20m"
         location = input_dir / name
@@ -82,12 +82,11 @@ class Movielens20MConverter(CsvConverter):
         links_df = read_csv(location, "links", file_type, sep, header)
         ratings_df = pd.merge(ratings_df, links_df)
 
-
         merged_df = pd.merge(ratings_df, movies_df).sort_values(
             by=[Movielens20MConverter.RATING_USER_COLUMN_NAME, Movielens20MConverter.RATING_TIMESTAMP_COLUMN_NAME])
 
-        # Remove unnecessary columns
-        merged_df = merged_df.drop('movieId', axis=1).drop('imdbId', axis=1).drop('tmdbId', axis=1)
+        # Remove unnecessary columns, we keep movieId here, so that we can filter later
+        merged_df = merged_df.drop('imdbId', axis=1).drop('tmdbId', axis=1)
 
         os.makedirs(output_file.parent, exist_ok=True)
 
@@ -128,6 +127,7 @@ class Movielens1MConverter(CsvConverter):
         os.makedirs(output_file.parent, exist_ok=True)
 
         merged_df.to_csv(output_file, sep=self.delimiter, index=False)
+
 
 class AmazonConverter(CsvConverter):
     AMAZON_SESSION_ID = "reviewer_id"
