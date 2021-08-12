ce531cbcea3af3e4cc9fa5c4f0beabe3619b181e
Alexander Dallmann <dallmann@informatik.uni-wuerzburg.de>
2021-08-12 12:10:28 +0200

    * make project directory discovery more robust

--- src/asme/utils/ioutils.py
+++ src/asme/utils/ioutils.py
@@ -1,11 +1,13 @@
 import json
 import os
 from pathlib import Path
-from typing import List, Callable, Any
+from typing import List, Callable, Any, Optional
 
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import LoggerCollection, LightningLoggerBase
 
+from asme.callbacks.best_model_writing_model_checkpoint import BestModelWritingModelCheckpoint
 from asme.init.config import Config
 
 
@@ -27,8 +29,11 @@ def save_config(config: Config,
         json.dump(config.config, f, indent=4)
 
 
-def _build_log_dir_from_logger(logger: LightningLoggerBase) -> Path:
+def _build_log_dir_from_logger(logger: LightningLoggerBase) -> Optional[Path]:
     # FIXME: remove method as soon as the is a common build method for this in the lightning lib
+    if logger.save_dir is None:
+        return None
+
     log_dir = Path(logger.save_dir)
     logger_name = logger.name
     if logger_name and len(logger_name) > 0:
@@ -43,21 +48,40 @@ def _build_log_dir_from_logger(logger: LightningLoggerBase) -> Path:
     return log_dir
 
 
-def determine_log_dir(trainer: Trainer
-                      ) -> Path:
-    """
-    Determines the logger directory used by the trainer in the following way:
-    1. If multiple loggers are specified the save directory of the first one is used.
-    2. If only a singe logger is registered, its log directory is used.
-    3. If the provided trainer has no attached loggers, its default_root_directory is used.
-    """
-    if trainer.logger is None:
-        return Path(trainer.default_root_dir)
-    if type(trainer.logger) is LoggerCollection:
-        # FIXME: the first logger can have no save_dir
-        return _build_log_dir_from_logger(trainer.logger[0])
+def determine_log_dir(trainer: Trainer) -> Path:
+    log_dir = None
 
-    return _build_log_dir_from_logger(trainer.logger)
+    # try to infer from the logger configuration
+    logger = trainer.logger
+    if type(logger) is LightningLoggerBase:
+        log_dir = _build_log_dir_from_logger(logger)
+    elif type(logger) is LoggerCollection:
+        for l in logger._logger_iterable:
+            dir = _build_log_dir_from_logger(l)
+            if dir is not None:
+                log_dir = dir
+                break
+    else:
+        print(f"Unable to infer log_dir from logger configuration. Failing over to ModelCheckpoint.")
+
+    # no logger provided a directory, e.g. mlflow is used
+    # fail over to model_checkpoint and use the parent directory
+    if log_dir is None:
+        ckpt_callback = trainer.checkpoint_callback
+        if ckpt_callback is not None:
+            if type(ckpt_callback) is ModelCheckpoint:
+                log_dir = Path(ckpt_callback.dirpath).parent
+            elif type(ckpt_callback) is BestModelWritingModelCheckpoint:
+                log_dir = Path(ckpt_callback.output_base_path).parent
+            else:
+                print(f"Could not infer output path from checkpoint callback of type: {type(ckpt_callback)}")
+
+    # querying logger / checkpoint callback failed: fallback to Trainer.default_root_dir
+    if log_dir is None:
+        log_dir = Path(trainer.default_root_dir)
+
+    print(log_dir)
+    return log_dir
 
 
 def save_finished_flag(path: Path,
