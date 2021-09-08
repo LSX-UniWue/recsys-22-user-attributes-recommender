046465e634a391029f0d49296be32c17a969544c
Johannes Kohlmann <johannes.kohlmann@stud-mail.uni-wuerzburg.de>
2021-09-08 15:40:09 +0200

    Cleaned up left-overs from the container factory.

--- src/asme/init/factories/container.py
+++ src/asme/init/factories/container.py
@@ -3,68 +3,17 @@ from typing import List
 from asme.init.config import Config
 from asme.init.container import Container
 from asme.init.context import Context
-from asme.init.factories.include.import_factory import ImportFactory
 from asme.init.factories.common.conditional_based_factory import ConditionalFactory
 from asme.init.factories.common.dependencies_factory import DependenciesFactory
+from asme.init.factories.data_sources.datamodule import DataModuleFactory
 from asme.init.factories.features.features_factory import FeaturesFactory
 from asme.init.factories.features.tokenizer_factory import TOKENIZERS_PREFIX
-from asme.init.factories.data_sources.datamodule import DataModuleFactory
-from asme.init.factories.modules.modules import GenericModuleFactory
+from asme.init.factories.include.import_factory import ImportFactory
 from asme.init.factories.trainer import TrainerBuilderFactory
 from asme.init.object_factory import ObjectFactory, CanBuildResult, CanBuildResultType
-from asme.losses.basket.dream.dream_loss import DreamContrastiveLoss
-from asme.losses.cosrec.cosrec_loss import CosRecLoss
-from asme.losses.hgn.hgn_loss import HGNLoss
-from asme.losses.sasrec.sas_rec_losses import SASRecBinaryCrossEntropyLoss
-from asme.models.basket.nnrec.nnrec_model import NNRecModel
-from asme.models.bert4rec.bert4rec_model import BERT4RecModel
-from asme.models.caser.caser_model import CaserModel
-from asme.models.cosrec.cosrec_model import CosRecModel
-from asme.models.narm.narm_model import NarmModel
-from asme.models.rnn.rnn_model import RNNModel
-from asme.models.sasrec.sas_rec_model import SASRecModel
-from asme.models.kebert4rec.kebert4rec_model import KeBERT4RecModel
-from asme.models.hgn.hgn_model import HGNModel
-from asme.modules.baselines.bpr_module import BprModule
-from asme.modules.baselines.markov_module import MarkovModule
-from asme.modules.baselines.pop_module import PopModule
-from asme.modules.baselines.session_pop_module import SessionPopModule
-from asme.modules.masked_training_module import MaskedTrainingModule
-from asme.modules.next_item_prediction_training_module import NextItemPredictionTrainingModule, \
-    NextItemPredictionWithNegativeSampleTrainingModule
 from asme.modules.registry import REGISTERED_MODULES
-from asme.modules.sequence_next_item_prediction_training_module import SequenceNextItemPredictionTrainingModule
 
-"""{'kebert4rec': GenericModuleFactory(MaskedTrainingModule,
-                                                                              model_cls=KeBERT4RecModel,
-                                                                              loss_function=None),
-                                           'bert4rec': GenericModuleFactory(MaskedTrainingModule,
-                                                                            model_cls=BERT4RecModel,
-                                                                            loss_function=None),
-                                           'caser': GenericModuleFactory(SequenceNextItemPredictionTrainingModule,
-                                                                         model_cls=CaserModel),
-                                           'narm': GenericModuleFactory(NextItemPredictionTrainingModule,
-                                                                        model_cls=NarmModel),
-                                           'sasrec': GenericModuleFactory(SequenceNextItemPredictionTrainingModule,
-                                                                          SASRecBinaryCrossEntropyLoss(),
-                                                                          SASRecModel),
-                                           'rnn': GenericModuleFactory(module_cls=NextItemPredictionTrainingModule,
-                                                                       model_cls=RNNModel),
-                                           'cosrec': GenericModuleFactory(SequenceNextItemPredictionTrainingModule,
-                                                                          CosRecLoss(),
-                                                                          CosRecModel),
-                                           'hgn': GenericModuleFactory(SequenceNextItemPredictionTrainingModule,
-                                                                       HGNLoss(),
-                                                                       HGNModel),
-                                           'dream': GenericModuleFactory(NextItemPredictionWithNegativeSampleTrainingModule,
-                                                                         DreamContrastiveLoss,
-                                                                         RNNModel),
-                                           'nnrec': GenericModuleFactory(module_cls=NextItemPredictionTrainingModule,
-                                                                         model_cls=NNRecModel),
-                                           'pop': GenericModuleFactory(PopModule, model_cls=None),
-                                           'session_pop': GenericModuleFactory(SessionPopModule, model_cls=None),
-                                           'markov': GenericModuleFactory(MarkovModule, model_cls=None),
-                                           'bpr': GenericModuleFactory(BprModule, model_cls=None)},"""
+
 class ContainerFactory(ObjectFactory):
     def __init__(self):
         super().__init__()
