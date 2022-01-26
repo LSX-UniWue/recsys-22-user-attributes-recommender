a008d478665a80d8f4ecbd6e13077ef2d1bb0acc
Johannes Kohlmann <johannes.kohlmann@stud-mail.uni-wuerzburg.de>
2022-01-26 10:07:01 +0100

    Injects are now available for modules and models.

--- src/asme/core/init/factories/modules/modules.py
+++ src/asme/core/init/factories/modules/modules.py
@@ -9,7 +9,7 @@ from asme.core.init.factories.metrics.metrics_container import MetricsContainerF
 from asme.core.init.factories.util import require_config_keys
 from asme.core.init.object_factory import ObjectFactory, CanBuildResult, CanBuildResultType
 from asme.core.tokenization.tokenizer import Tokenizer
-from asme.core.utils.inject import InjectTokenizer, InjectTokenizers, InjectVocabularySize, InjectModel
+from asme.core.utils.inject import InjectTokenizer, InjectTokenizers, InjectVocabularySize, InjectModel, Inject
 
 MODEL_PARAM_NAME = 'model'
 METRICS_PARAM_NAME = 'metrics'
@@ -22,26 +22,34 @@ MODEL_SUFFIX = '_' + MODEL_PARAM_NAME
 @dataclass
 class ParameterInfo:
     """ class to store information about the parameter """
+    parameter_name: str
     parameter_type: type
     default_value: Optional[Any] = None
 
+    def get_inject_instance(self) -> Optional[Inject]:
+        return self.parameter_type if self.is_injectable() else None
 
-def _get_parameters(cls) -> Dict[str, ParameterInfo]:
+    def is_injectable(self):
+        return isinstance(self.parameter_type, Inject)
+
+
+def _get_parameters(cls) -> List[ParameterInfo]:
     signature = inspect.signature(cls.__init__)
 
-    parameter_infos = {}
+    parameter_infos = []
 
     for parameter_name, info in signature.parameters.items():
         if parameter_name != 'self':
-            parameter_infos[parameter_name] = ParameterInfo(info.annotation, info.default)
+            parameter_infos += [ParameterInfo(parameter_name, info.annotation, info.default)]
 
     return parameter_infos
 
 
-def _filter_parameters(parameters: Dict[str, ParameterInfo],
-                       filter_func: Callable[[], bool]
-                       ) -> Dict[str, Optional[Any]]:
-    return dict(filter(filter_func, parameters.items()))
+def _filter_parameters(parameters: List[ParameterInfo],
+                       filter_func: Callable[[ParameterInfo], bool]
+                       ) -> List[ParameterInfo]:
+    return list(filter(filter_func, parameters))
+
 
 def _get_tokenizers_from_context(context: Context) -> Dict[str, Tokenizer]:
     tokenizers = {}
@@ -50,6 +58,62 @@ def _get_tokenizers_from_context(context: Context) -> Dict[str, Tokenizer]:
             tokenizers[key.replace(TOKENIZER_SUFFIX, '')] = item
 
     return tokenizers
+
+
+def _handel_injects(injectable_parameters: List[ParameterInfo], context: Context, config: Config,
+                    parameter_dict: Dict[str, Any]):
+    """
+    This function tries to inject a value into the parameter_dict for every parameter given by iunjectable parameters.
+
+    :param injectable_parameters: A list of parameter info that are injectable, i.e. parameter infos
+    p where p.is_injectable() == True
+    :param context: The context the current factory is working in.
+    :param config: The Config object used to build nested models from.
+    :param parameter_dict: A dictionary of parameter names and values that should be populated by values gathered from
+    the context or the config.
+    """
+    for injectable_parameter in injectable_parameters:
+        inject = injectable_parameter.get_inject_instance()
+
+        if isinstance(inject, InjectTokenizer):
+            tokenizer_to_use = inject.feature_name
+            tokenizer = context.get(get_tokenizer_key_for_voc(tokenizer_to_use))
+
+            if tokenizer is None:
+                if injectable_parameter.default_value == inspect._empty:
+                    raise KeyError(f'No tokenizer with id "{tokenizer_to_use}" configured and no default value set.')
+                else:
+                    tokenizer = injectable_parameter.default_value
+            parameter_dict[injectable_parameter.parameter_name] = tokenizer
+        elif isinstance(inject, InjectTokenizers):
+            tokenizers = _get_tokenizers_from_context(context)
+            parameter_dict[injectable_parameter.parameter_name] = tokenizers
+        elif isinstance(inject, InjectVocabularySize):
+            tokenizer_to_use = inject.feature_name
+            tokenizer = context.get(get_tokenizer_key_for_voc(tokenizer_to_use))
+            if tokenizer is None:
+                raise KeyError(f"No tokenizer with id {tokenizer_to_use} configured. "
+                               f"Can not inject vocabulary size into parameter {injectable_parameter.parameter_name}.")
+            else:
+                parameter_dict[injectable_parameter.parameter_name] = len(tokenizer)
+        elif isinstance(inject, InjectModel):
+            config_section_name = inject.config_section_name \
+                if inject.config_section_name is not None else injectable_parameter.parameter_name
+            if not config.has_path([config_section_name]):
+                if injectable_parameter.default_value == inspect._empty:
+                    raise KeyError(
+                        f"A sub-model '{injectable_parameter.parameter_name}' of type "
+                        f"'{inject.model_cls}' with no default value was specified but no configuration section "
+                        f"named '{config_section_name}' was found in the config.")
+            else:
+                factory = GenericModelFactory(inject.model_cls)
+                # We assume that the config entry has the same name as the model parameter
+                model_config = config.get_config([config_section_name])
+                model = factory.build(model_config, context)
+                parameter_dict[injectable_parameter.parameter_name] = model
+        else:
+            # We failed ot handle an inject case!
+            raise NotImplementedError(f"Inject instances of type {type(inject)} are not handled yet!")
 
 
 class GenericModuleFactory(ObjectFactory):
@@ -94,45 +158,29 @@ class GenericModuleFactory(ObjectFactory):
 
         # collect the parameters from the config
         parameters = _get_parameters(self._module_csl)
-        tokenizer_parameters = _filter_parameters(parameters,
-                                                  lambda item: isinstance(item[1].parameter_type, InjectTokenizer))
-        config_parameters = dict([x for x in parameters.items() if x[0] not in tokenizer_parameters])
+        config_parameters = _filter_parameters(parameters, lambda p: not p.is_injectable())
 
         # Model is only present for modules that contain a model.
         if self.should_build_model:
-            config_parameters.pop(MODEL_PARAM_NAME)
+            config_parameters = _filter_parameters(config_parameters,
+                                                   lambda p: not p.parameter_name == MODEL_PARAM_NAME)
 
-        config_parameters.pop(METRICS_PARAM_NAME)
+        # We do not want to build the metrics container directly from the config.
+        config_parameters = _filter_parameters(config_parameters, lambda p: not p.parameter_name == METRICS_PARAM_NAME)
 
-        for parameter, parameter_info in config_parameters.items():
+        for parameter_info in config_parameters:
+            name = parameter_info.parameter_name
             default_value = parameter_info.default_value
             default_value = None if default_value == inspect._empty else default_value
-            named_parameters[parameter] = config.get_or_default(parameter, default_value)
-
-        # bind the tokenizers
-        for tokenizer_parameter_name, tokenizer_parameter_info in tokenizer_parameters.items():
-            inject_instance = tokenizer_parameter_info.parameter_type
-            tokenizer_to_use = inject_instance.feature_name
-            tokenizer = context.get(get_tokenizer_key_for_voc(tokenizer_to_use))
-
-            if tokenizer is None:
-                if tokenizer_parameter_info.default_value == inspect._empty:
-                    raise KeyError(f'No tokenizer with id "{tokenizer_to_use}" configured and no default value set.')
-                else:
-                    tokenizer = tokenizer_parameter_info.default_value
-            named_parameters[tokenizer_parameter_name] = tokenizer
-
-        # if requested, bind all tokenizers to a single variable as a dict indexed by the feature keys
-        tokenizers_parameters = _filter_parameters(parameters,
-                                                   lambda item: isinstance(item[1].parameter_type, InjectTokenizers))
-        if len(tokenizers_parameters) > 0:
-            tokenizers = _get_tokenizers_from_context(context)
-            for name, info in tokenizers_parameters.items():
-                named_parameters[name] = tokenizers
+            named_parameters[name] = config.get_or_default(name, default_value)
 
         # build the metrics container
         metrics = self.metrics_container_factory.build(config.get_config(self.metrics_container_factory.config_path()),
                                                        context=context)
+
+        # handle Inject directives
+        injectable_parameters = _filter_parameters(parameters, lambda p: p.is_injectable())
+        _handel_injects(injectable_parameters, context, config, named_parameters)
 
         # build the model container if a model class was supplied
         if self.should_build_model:
@@ -181,45 +229,15 @@ class GenericModelFactory(ObjectFactory):
 
         # collect the parameters from the config
         parameters = _get_parameters(self._model_cls)
-        for parameter, parameter_info in parameters.items():
+        for parameter_info in parameters:
             default_value = parameter_info.default_value
+            parameter_name = parameter_info.parameter_name
             default_value = None if default_value == inspect._empty else default_value
-            named_parameters[parameter] = config.get_or_default(parameter, default_value)
+            named_parameters[parameter_name] = config.get_or_default(parameter_name, default_value)
 
-        vocab_vars = _filter_parameters(parameters,
-                                        lambda dict_item: isinstance(dict_item[1].parameter_type, InjectVocabularySize))
-
-        # Collect parameters that are a model themselves
-        model_params = _filter_parameters(parameters, lambda param: isinstance(param[1].parameter_type, InjectModel))
-        if len(model_params) > 0:
-            # Build all parameter models recursively
-            for model_param_name, model_param_info in model_params.items():
-                inject_instance = model_param_info.parameter_type
-                config_section_name = inject_instance.config_section_name \
-                    if inject_instance.config_section_name is not None else model_param_name
-                if not config.has_path([config_section_name]):
-                    if model_param_info.default_value == inspect._empty:
-                        raise KeyError(
-                            f"Model '{self._model_cls.__name__}' specifies a sub-model '{model_param_name}' of type "
-                            f"'{inject_instance.model_cls}' with no default value but no configuration section "
-                            f"named '{config_section_name}' was found in the config.")
-                else:
-                    factory = GenericModelFactory(model_param_info.parameter_type)
-                    # We assume that the config entry has the same name as the model parameter
-                    model_config = config.get_config([config_section_name])
-                    model = factory.build(model_config, context)
-                    named_parameters[model_param_name] = model
-
-        for name, info in vocab_vars.items():
-            inject_instance = info.parameter_type
-            tokenizer_to_use = inject_instance.feature_name
-            tokenizer = context.get(get_tokenizer_key_for_voc(tokenizer_to_use))
-
-            vocab_size = 0
-            if tokenizer is not None:
-                vocab_size = len(tokenizer)
-
-            named_parameters[name] = vocab_size
+        # handle Inject directives
+        injectable_parameters = _filter_parameters(parameters, lambda p: p.is_injectable())
+        _handel_injects(injectable_parameters, context, config, named_parameters)
 
         return self._model_cls(**named_parameters)
 
@@ -233,11 +251,12 @@ class GenericModelFactory(ObjectFactory):
         return self._model_cls.__name__.lower()
 
 
-def _get_config_required_config_params(parameters: Dict[str, Optional[Any]]) -> List[str]:
+def _get_config_required_config_params(parameters: List[ParameterInfo]) -> List[str]:
     result = []
 
-    for parameter_name, parameter_info in parameters.items():
+    for parameter_info in parameters:
         default_value = parameter_info.default_value
+        parameter_name = parameter_info.parameter_name
         if default_value is inspect._empty and not parameter_name.endswith(VOCAB_SIZE_SUFFIX):
             result.append(parameter_name)
 
